23003b3e95118aeaeb3fff0d7d95c7f4e0ecb910
AstridLancrey <astrid.lancrey@gmail.com>
2021-01-05 20:45:20 +0100

    added Adding_and_editing_FilesToRepo.sh

--- Adding_and_editing_FilesToRepo.sh
+++ Adding_and_editing_FilesToRepo.sh
@@ -1,29 +1,19 @@
 ### Ajout de fichiers dans un repository gitHub ###
 
-## en supposant que le juypyter notebook Keras_digits ait été rajouté en local
-# git add Keras_digits.ipynb
-# git status
-# git commit -m "added Keras_digits.ipynb"
-# git push
-
-## pareil pour ajouter le fichier GithubCreateRepo.sh:
-# git add GithubCreateRepo.sh
-# git status
-# git commit -m "GithubCreateRepo.sh"
-# git push
-
-##
-# git add AddingFilesToRepo.sh
-# git status
-# git commit -m "AddingFilesToRepo.sh"
-# git push
-
-## si on veut updtater un fichier modifié en local:
+## en supposant chaque fichier ait été rajouté en local
 
 git add Keras_digits.ipynb
 git commit -m "added Keras_digits.ipynb"
+# git push
 git push  -f origin master
 
 git add Adding_and_editing_FilesToRepo.sh
 git commit -m "added Adding_and_editing_FilesToRepo.sh"
 git push  -f origin master
+
+git add GithubCreateRepo.sh
+git commit -m "added GithubCreateRepo.sh"
+git push  -f origin master
+
+## si on veut updtater un fichier modifié en local:
+## il suffit de re taper les 3 commandes pour le fichier correspondant
